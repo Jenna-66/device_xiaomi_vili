68db0146f6ceaddd4eee6b340b686397c5fb3c28
0mar99 <omarag9099@gmail.com>
2025-03-16 14:31:39 -0600

    vili: Pixel-ify!
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- aosp_vili.mk
+++ aosp_vili.mk
@@ -8,17 +8,31 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from haydn device
+# Inherit from vili device
 $(call inherit-product, device/xiaomi/vili/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixel Project stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Pixel Project flags
+CUSTOM_BUILD_TYPE := UNOFFICIAL
+CUSTOM_MAINTAINER := 0mar
+
+# Gapps Flag
+WITH_GMS := true
+
+# Extras
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := vili
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2107113SG
-PRODUCT_NAME := lineage_vili
+PRODUCT_NAME := aosp_vili
 
 PRODUCT_SYSTEM_NAME := vili_global
 PRODUCT_SYSTEM_DEVICE := vili
