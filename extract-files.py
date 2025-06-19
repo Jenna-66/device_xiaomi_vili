2267837c660898a8faccc524be1caf4429a3161a
johnmart19 <johnivan19999@gmail.com>
2025-06-19 10:12:38 +0300

    vili: Patch audio.primary.lahaina & libaudioroute_ext for VNDK 34
    VNDK 36 crashing:
    Cmdline: /vendor/bin/hw/android.hardware.audio.service
    pid: 1093, tid: 1180, name: HwBinder:1093_2  >>> /vendor/bin/hw/android.hardware.audio.service <<<
    uid: 1041
    signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x00000040
    Cause: null pointer dereference
        r0  00000000  r1  f3cc0730  r2  00000003  r3  00000003
        r4  f418f780  r5  00000024  r6  00000001  r7  00000000
        r8  f3aacbf4  r9  f3f8600c  r10 f418f780  r11 f418f780
        ip  f2166a38  sp  f27ff4f8  lr  f216409f  pc  f2133588
    
    Requires https://github.com/DerpFest-AOSP/android_hardware_lineage_compat/commit/d3840b5dd032e2650df739256fd376c844a3bed9 to work
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -44,6 +44,10 @@ blob_fixups: blob_fixups_user_type = {
          .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/vendor.xiaomi.hardware.cameraperf@1.0-impl.so': blob_fixup()
         .sig_replace('21 00 80 52 7c 00 00 94', '21 00 80 52 1F 20 03 D5'),
+    'vendor/lib/libaudioroute_ext.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
+    'vendor/lib/hw/audio.primary.lahaina.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
