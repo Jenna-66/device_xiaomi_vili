fefc4eba84db6a937ca5e1b39180069a1560bd4f
Jenna-66 <jenna@jennalody.de>
2026-03-01 15:03:05 +0100

    Revert "vili: Derp-ify!"
    This reverts commit ceb20b8285676fe0927371e753151d568f1e3ca7.
    
    Change-Id: I247511f5a4644c8d17e015cd648b68e176f4a14d

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -9,13 +9,6 @@ $(call inherit-product, device/xiaomi/vili/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# DerpFest Flags
-DERPFEST_BUILD_TYPE := Official
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Gapps Flag
-WITH_GMS := true
-
 # Extras
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_ENABLE_BLUR := true
